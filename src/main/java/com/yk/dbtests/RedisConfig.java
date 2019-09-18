e877c187f56fefa7e306bb1d93eb56377c5807d2
Yosep Kim <yosepk@gmail.com>
2019-09-18 12:57:44 -0400

    force ssl

--- src/main/java/com/yk/dbtests/RedisConfig.java
+++ src/main/java/com/yk/dbtests/RedisConfig.java
@@ -3,6 +3,7 @@ package com.yk.dbtests;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -12,7 +13,11 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(
+                "ua3167c3c775.redis.cache.windows.net", 6380);
+        config.setPassword("XGdN0ehQ0HHAzlWBr3vHeJBu1Gn0X8Q7PIVtzb612IE=");
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
